1095ea558b16a88e98446697eeca55fcb280b51f
Alex Wolf <f.alexander.wolf@gmail.com>
2022-08-06 22:39:53 +0200

    :construction_worker: Fix trailing slash

--- docs/conf.py
+++ docs/conf.py
@@ -6,11 +6,13 @@ HERE = Path(__file__).parent
 sys.path[:0] = [str(HERE), str(HERE.parent)]
 
 from lamin_sphinx import *  # noqa
-from lamin_sphinx import authors, html_context  # noqa
+from lamin_sphinx import authors, html_theme_options, html_context  # noqa
 
 project = "Lamin Reports"
 html_title = f"{project}"
 html_context["github_repo"] = "lamin-reports"  # noqa
+# We're actually using this for the link behind the brand of the page!
+html_theme_options["logo"] = {"link": "reports"}  # noqa
 
 # Blog
 blog_baseurl = "https://lamin.ai/reports"
