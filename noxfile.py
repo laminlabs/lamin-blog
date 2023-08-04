017b998adf903d4dfacfe5815c1730049ea9ce97
Alex Wolf <f.alexander.wolf@gmail.com>
2023-08-04 18:44:56 +0200

    ♻️ Refactor docs build (#13)
    * ♻️ Refactor docs build
    
    * ♻️ Refactor github workflow
    
    * 💚 Fix

--- noxfile.py
+++ noxfile.py
@@ -1,19 +1,14 @@
-from pathlib import Path
-
 import nox
+from laminci.nox import build_docs, run_pre_commit
 
-nox.options.reuse_existing_virtualenvs = True
+nox.options.default_venv_backend = "none"
 
 
 @nox.session
 def lint(session: nox.Session) -> None:
-    session.install("pre-commit")
-    session.run("pre-commit", "install")
-    session.run("pre-commit", "run", "--all-files")
+    run_pre_commit(session)
 
 
-@nox.session(python=["3.7", "3.8", "3.9", "3.10", "3.11"])
-def build(session):
-    prefix = "." if Path("./lndocs").exists() else ".."
-    session.install(f"{prefix}/lndocs")
-    session.run("lndocs")
+@nox.session
+def build(session: nox.Session):
+    build_docs(session, strict=True)
