a82d1f627f167f89167309610f986198fbc79ea1
Alex Wolf <f.alexander.wolf@gmail.com>
2024-04-01 22:44:57 +0200

    🍱 Add authors here

--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,7 @@ HERE = Path(__file__).parent
 sys.path[:0] = [str(HERE), str(HERE.parent)]
 
 from lamin_sphinx import *  # noqa
-from lamin_sphinx import authors, html_theme_options, html_context  # noqa
+from lamin_sphinx import html_theme_options, html_context  # noqa
 
 project = "Lamin Blog"
 html_title = f"{project}"
@@ -29,6 +29,24 @@ html_theme_options["icon_links"] = [
 ]
 
 # Blog
+
+authors = {
+    "bpenteado": ("Bernardo Penteado", "https://pbern.com"),
+    "chaichontat": ("Chaichontat Sriworarat", "https://github.com/chaichontat"),
+    "falexwolf": ("Alex Wolf", "https://falexwolf.me"),
+    "felix-fischer*": ("Felix Fischer*", ""),
+    "maciek-wiatrak": ("Maciek Wiatrak", ""),
+    "ilan-gold": ("Ilan Gold", ""),
+    "fabian-theis": ("Fabian Theis", ""),
+    "jkobject": ("Jeremie Kalfon", "https://www.jkobject.com"),
+    "yanay-rosen": ("Yanay Rosen", ""),
+    "fredericenard": ("Frederic Enard", "https://github.com/fredericenard"),
+    "Koncopd": ("Sergei Rybakov", "https://github.com/Koncopd"),
+    "Koncopd*": ("Sergei Rybakov*", "https://github.com/Koncopd"),
+    "sunnyosun": ("Sunny Sun", "https://github.com/sunnyosun"),
+    "Zethson": ("Lukas Heumos", "https://github.com/Zethson"),
+}
+
 blog_baseurl = "https://lamin.ai/blog"
 blog_post_pattern = "*"
 blog_authors = authors
