faae1d76606531aaf0030bc083841f090be12615
zethson <lukas.heumos@posteo.net>
2024-05-01 15:23:26 +0200

    Revert to pip
    Signed-off-by: zethson <lukas.heumos@posteo.net>

--- noxfile.py
+++ noxfile.py
@@ -1,5 +1,4 @@
 import nox
-from pathlib import Path
 from laminci import move_built_docs_to_slash_project_slug
 from laminci.nox import run_pre_commit
 
@@ -13,11 +12,6 @@ def lint(session: nox.Session) -> None:
 
 @nox.session
 def build(session: nox.Session):
-    prefix = "." if Path("./lndocs").exists() else ".."
-    if nox.options.default_venv_backend == "none":
-        session.run(*f"uv pip install --system {prefix}/lndocs".split())
-    else:
-        session.install(f"{prefix}/lndocs")
-
+    session.run(*"pip install ./lndocs".split())
     session.run(*["lndocs", "--strict"])
     move_built_docs_to_slash_project_slug()
